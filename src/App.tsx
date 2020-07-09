d1b385ca68c91eef74fb3eac7c6f8ed13825fc91
Shawn Rivers <kaushou@gmail.com>
2020-07-09 22:53:50 +0900

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'></header>
+    </div>
+  );
+};
+
+export default App;
